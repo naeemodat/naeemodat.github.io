7e4b4fb6675d8f06ccc1538c7740d3ff778cb813
naeemodat <naeemodat@gmail.com>
2024-07-12 14:39:54 +0300

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,8 +1,11 @@
-$(function(){
-  $(".schedule").click(function(){
+$(function () {
+  $(".schedule").click(function () {
     $("#content").load("schedule.html");
   });
-  $("#home").click(function(){
+  $("#home").click(function () {
     $("#content").load("home.html");
-  })
+  });
+  $(".cpre").click(function () {
+    $("#content").load("cpre.html");
+  });
 });
